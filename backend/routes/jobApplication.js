5802b8b48f3f096e029f3bd9de4997a89ece4ab5
daviezu <1david.hng@gmail.com>
2024-08-28 15:58:30 +0700

    update jobApplication routing for recruiter & talent

--- backend/routes/jobApplication.js
+++ backend/routes/jobApplication.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { applyJob, getAllJob } = require("../controllers/jobApplication");
+const {
+  applyJob,
+  getAllJob,
+  getListApplicant,
+} = require("../controllers/jobApplication");
 const isAuth = require("../middleware/isAuth");
 const checkRole = require("../middleware/checkRole");
 const router = express.Router();
 
+// talent
+router.get("/", isAuth, checkRole("Talent"), getAllJob);
 router.post("/", applyJob);
-router.get("/", getAllJob);
+
+// recruiter
+router.get("/company", isAuth, checkRole("Recruiter"), getListApplicant);
 
 module.exports = router;
