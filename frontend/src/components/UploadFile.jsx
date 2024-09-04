e72273ef3ad698a7664b32f94a3a9f06df7c06d1
vnnico <nicson7585@gmail.com>
2024-09-04 23:53:37 +0700

    feat: pdf viewer

--- frontend/src/components/UploadFile.jsx
+++ frontend/src/components/UploadFile.jsx
@@ -1,37 +1,60 @@
 import React from "react";
 import { InboxOutlined } from "@ant-design/icons";
 import { message, Upload } from "antd";
+import { useMutation } from "@tanstack/react-query";
+import * as apiClient from "../api-client";
+
 const { Dragger } = Upload;
 
-const props = {
-  name: "file",
-  multiple: false,
-  action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
+const UploadFile = () => {
+  const mutation = useMutation({
+    mutationKey: ["upload"],
+    mutationFn: apiClient.analyzeCV,
+    onSuccess: (data, variables) => {
+      message.success(`${variables.name} file uploaded successfully.`);
+    },
+    onError: (error, variables) => {
+      message.error(`${variables.name} file upload failed.`);
+      console.error("Error uploading file:", error);
+    },
+  });
+  const uploadProps = {
+    name: "file",
+    multiple: false,
+    customRequest: ({ file, onSuccess, onError }) => {
+      mutation.mutate(file, {
+        onSuccess: () => {
+          onSuccess("Ok");
+        },
+        onError: (error) => {
+          onError(error);
+        },
+      });
+    },
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
+  return (
+    <Dragger {...uploadProps}>
+      <p className="ant-upload-drag-icon align-center">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Upload your CV in PDF format to begin your AI-powered analysis.
+      </p>
+    </Dragger>
+  );
 };
 
-const UploadFile = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon  align-center">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">
-      Upload your CV in PDF format to begin your AI-powered analysis.
-    </p>
-  </Dragger>
-);
 export default UploadFile;
